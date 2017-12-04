abc7f9469207b234466db1d4d35bc26d03a549b1
Krum <krum.yanko.3@gmail.com>
2017-12-04 23:57:57 +0100

    listening to project events in firebase

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -13,20 +13,31 @@ export class ProjectService {
   constructor(private ngRedux: NgRedux<InitialAppState>, private actions: ProjectActions) {
     this.db = Firebase.database();
     this.dbRefs = DbRefs;
+    this.listenForChanges();
    }
 
    fetchProjects(): void {
      console.log('should fetch projects');
      this.db.ref('/projects/').once('value', (snapshot) => {
-       console.log('inside snapshot');
+       console.log('snapshot');
+       console.log(snapshot);
        snapshot.forEach(project => {
          const fetchedProject = project.val();
          console.log('fetched project');
          console.log(fetchedProject);
-         this.ngRedux.dispatch(
-           this.actions.addProject(new Project(fetchedProject.name, fetchedProject.id, fetchedProject.description, null, null)));
+        //  this.ngRedux.dispatch(
+        //    this.actions.addProject(new Project(fetchedProject.name, fetchedProject.id, fetchedProject.description, null, null)));
        });
      });
+   }
+
+   listenForChanges(): void {
+     this.db.ref('/projects/').on('child_added', (data) => {
+       const newProject = data.val();
+       this.ngRedux.dispatch(
+         this.actions.addProject(new Project(newProject.name, newProject.id, newProject.description, null, null)));
+      });
+     }
    }
 
 }
