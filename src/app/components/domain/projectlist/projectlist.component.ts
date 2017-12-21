aa0073a3c329374b81ee959d1fb2d39778af63c0
Krum <krum.yanko.3@gmail.com>
2017-12-21 22:05:59 +0100

    filter projects by client

--- src/app/components/domain/projectlist/projectlist.component.ts
+++ src/app/components/domain/projectlist/projectlist.component.ts
@@ -42,6 +42,22 @@ export class ProjectlistComponent implements OnInit {
     }
   }
 
+  updateClientFilterTerm(clientName: string) {
+    this.projects = JSON.parse(JSON.stringify(this.nonFilteredProjects));
+    this.projects = this.projects.filter(project => {
+      if (project.client) {
+        return clientName === '' ? true : project.client.name === clientName;
+      } else {
+        return false;
+      }
+    });
+  }
+
+  clearClientFilterTerm(clear: boolean) {
+    if (clear) {
+      this.projects = JSON.parse(JSON.stringify(this.nonFilteredProjects));
+    }
+  }
   openNewProjectModal() {
     this.ngRedux.dispatch(this.modalActions.openModal(ModalTypes.ADD_NEW_PROJECT));
   }
--- src/app/components/interaction/filters/project-filter/project-filter.component.ts
+++ src/app/components/interaction/filters/project-filter/project-filter.component.ts
@@ -1,22 +1,50 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
+import { Client } from '../../../../models/client';
+import { NgRedux } from '@angular-redux/store';
+import { InitialAppState } from '../../../../store/initialState';
+
+import { ClientsService } from '../../../../clients.service';
+import { Subscription } from 'rxjs/Subscription';
+import { OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-project-filter',
   templateUrl: './project-filter.component.html',
   styleUrls: ['./project-filter.component.css']
 })
-export class ProjectFilterComponent implements OnInit {
-  filtersVisible: Boolean = true;
+export class ProjectFilterComponent implements OnInit, OnDestroy {
+  filtersVisible: Boolean = false;
   projectName: string;
+  private clientsSubscription: Subscription = new Subscription();
+  selectedClient: string;
+  clients: Client[];
   @Output() onNameFilter: EventEmitter<string> = new EventEmitter<string>();
   @Output() onClearNameFilter: EventEmitter<boolean> = new EventEmitter<boolean>();
-  constructor() {
+  @Output() onClientFilter: EventEmitter<string> = new EventEmitter<string>();
+  @Output() onClearClientFilter: EventEmitter<boolean> = new EventEmitter<boolean>();
+  constructor(
+    private store: NgRedux<InitialAppState>,
+    private clientsService: ClientsService) {
     this.projectName = '';
   }
 
   ngOnInit() {
+    this.fetchAvailableClients();
+    this.setUpClientsSubscription();
   }
+  ngOnDestroy() {
+    this.clientsSubscription.unsubscribe();
+  }
+  fetchAvailableClients(): void {
+    this.clientsService.getClients();
+  }
+  setUpClientsSubscription(): void {
+    this.clientsSubscription = this.store.select('clientsList').subscribe((clientsList: Client[]) => {
+      this.clients = clientsList;
+    });
+  }
+
   clearProjectName() {
     this.projectName = '';
     this.clearProjectsFilter();
@@ -36,5 +64,13 @@ export class ProjectFilterComponent implements OnInit {
     } else {
       this.clearProjectsFilter();
     }
+  }
+  submitClientFilter() {
+    this.onClientFilter.emit(this.selectedClient);
+  }
+
+  clearFilters() {
+    this.clearProjectName();
+    this.clearProjectsFilter();
   }
 }
